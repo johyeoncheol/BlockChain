667250dfa2668c57cf39e2580369f47992efa4ee
johyeoncheol <50797070+johyeoncheol@users.noreply.github.com>
2021-07-02 12:53:55 +0900

    Add files via upload

---
+++ JAVABlockChain/Block.java
@@ -0,0 +1,40 @@
+package com.blockchain;
+
+import java.util.Date;
+
+public class Block {
+	public String hash;
+	public String previousHash; 
+	private String data; 
+	private long timeStamp; 
+	private int nonce;
+	
+	//Block Constructor.  
+	public Block(String data,String previousHash ) {
+		this.data = data;
+		this.previousHash = previousHash;
+		this.timeStamp = new Date().getTime();
+		
+		this.hash = calculateHash();
+	}
+	
+	//Calculate new hash based on blocks contents
+	public String calculateHash() {
+		String calculatedhash = StringUtil.applySha256( 
+				previousHash +
+				Long.toString(timeStamp) +
+				Integer.toString(nonce) + 
+				data 
+				);
+		return calculatedhash;
+	}
+	
+	//Increases nonce value until hash target is reached.
+	public void mineBlock(int difficulty) {
+		String target = StringUtil.getDificultyString(difficulty); //Create a string with difficulty * "0" 
+		while(!hash.substring( 0, difficulty).equals(target)) {
+			nonce ++;
+			hash = calculateHash();
+		}
+	}
+}
---
+++ JAVABlockChain/chain.java
@@ -0,0 +1,58 @@
+package com.blockchain;
+
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+
+public class chain {
+	
+	public static ArrayList<Block> blockchain = new ArrayList<Block>();
+	public static int difficulty = 5;
+
+	public static void main(String[] args) {	
+		//add our blocks to the blockchain ArrayList:
+		
+		addBlock(new Block("Genesis Block", "0"));
+		
+		addBlock(new Block("2nd",blockchain.get(blockchain.size()-1).hash));
+		
+		addBlock(new Block("3rd",blockchain.get(blockchain.size()-1).hash));	
+		
+	
+		String blockchainJson = StringUtil.getJson(blockchain);
+		System.out.println(blockchainJson);
+	}
+	
+	public static Boolean isChainValid() {
+		Block currentBlock; 
+		Block previousBlock;
+		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+		
+		//loop through blockchain to check hashes:
+		for(int i=1; i < blockchain.size(); i++) {
+			currentBlock = blockchain.get(i);
+			previousBlock = blockchain.get(i-1);
+			//compare registered hash and calculated hash:
+			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+				System.out.println("Current Hashes not equal");			
+				return false;
+			}
+			//compare previous hash and registered previous hash
+			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+				System.out.println("Previous Hashes not equal");
+				return false;
+			}
+			//check if hash is solved
+			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+				System.out.println("This block hasn't been mined");
+				return false;
+			}
+			
+		}
+		return true;
+	}
+	
+	public static void addBlock(Block newBlock) {
+		newBlock.mineBlock(difficulty);
+		blockchain.add(newBlock);
+	}
+}
